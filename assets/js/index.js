2fcc516dd9ed7367295086fbdc0e2c58df33b318
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-29 15:19:28 +0000

    test fetch and test different ids

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,8 +8,6 @@ const timeout = function (s) {
     });
 };
 
-console.log('test')
-
 // https://forkify-api.herokuapp.com/v2
 // f5b6c833-b839-4f07-a521-e6fffdbc68d5
 
@@ -17,10 +15,31 @@ console.log('test')
 
 async function showRecipe() {
     try {
-        const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886')
+        const response = await fetch(
+            'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+            // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+        )
+        const data = await response.json();
 
+        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+        console.log(response, data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        }
+        console.log(recipe)
     } catch (error) {
         alert(error.message);
     }
 }
 
+
+showRecipe();
