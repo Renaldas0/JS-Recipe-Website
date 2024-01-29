fc576a82afb927d41b6cf287eddcc6749016a006
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-29 10:44:21 +0000

    set up api key

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,19 @@ const timeout = function (s) {
     });
 };
 
+console.log('test')
+
 // https://forkify-api.herokuapp.com/v2
 // f5b6c833-b839-4f07-a521-e6fffdbc68d5
 
 ///////////////////////////////////////
+
+async function showRecipe() {
+    try {
+        const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886')
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
---
+++ dist/index.ddbb3570.js
@@ -0,0 +1,21 @@
+const recipeContainer = document.querySelector(".recipe");
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+console.log("test");
+// https://forkify-api.herokuapp.com/v2
+// f5b6c833-b839-4f07-a521-e6fffdbc68d5
+///////////////////////////////////////
+async function showRecipe() {
+    try {
+        const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+//# sourceMappingURL=index.ddbb3570.js.map
