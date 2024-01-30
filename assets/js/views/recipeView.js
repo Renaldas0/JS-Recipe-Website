589b57bad36652d0144588dd60332f91ce9c0ed0
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-30 13:15:45 +0000

    refactor recipeView

--- assets/js/views/recipeView.js
+++ assets/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../images/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
@@ -81,20 +82,7 @@ class RecipeView {
         <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-                ${this.#data.ingredients.map(ingredient => {
-            return `
-                    <li class="recipe__ingredient">
-                    <svg class="recipe__icon">
-                    <use href="${icons}#icon-check"></use>
-                    </svg>
-                    <div class="recipe__quantity">${ingredient.quantity}</div>
-                    <div class="recipe__description">
-                    <span class="recipe__unit">${ingredient.unit}</span>
-                    ${ingredient.description}
-                    </div>
-                </li>
-                    `
-        }).join('')}
+                ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
 
             </ul>
         </div>
@@ -119,6 +107,22 @@ class RecipeView {
         </div>
       `;
     }
+
+    #generateMarkupIngredient(ingredient) {
+        return `
+                    <li class="recipe__ingredient">
+                    <svg class="recipe__icon">
+                    <use href="${icons}#icon-check"></use>
+                    </svg>
+                    <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
+                    <div class="recipe__description">
+                    <span class="recipe__unit">${ingredient.unit}</span>
+                    ${ingredient.description}
+                    </div>
+                </li>
+                    `
+    }
+
 }
 
 export default new RecipeView();
